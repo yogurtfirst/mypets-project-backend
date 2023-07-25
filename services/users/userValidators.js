ec73fe6f773937deae8a878c4049a2f5fb1fda69
yogurtfirst <ovolot@gmail.com>
2023-07-25 20:02:15 +0300

    Add regex validation

--- services/users/userValidators.js
+++ services/users/userValidators.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
-
-const PASSWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,16})/
+const { PASSWD_REGEX, DATE_REGEX, PHONE_REGEX } = require('../../constants/regexValues')
 
 exports.registerUserDataValidator = (data) =>
   Joi.object()
@@ -17,8 +16,8 @@ exports.updateUserDataValidator = (data) =>
       .options({ abortEarly: false })
       .keys({
         name: Joi.string().min(2).max(16),
-        birthday: Joi.string().length(10),
-        phone: Joi.string().min(10).max(13),
+        birthday: Joi.string().regex(DATE_REGEX),
+        phone: Joi.string().regex(PHONE_REGEX),
         city: Joi.string().min(2).max(30),
       })
       .validate(data)
