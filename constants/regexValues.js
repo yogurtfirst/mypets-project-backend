a5d87b372bf65e6e1484b884820ed9eb084a4c95
yogurtfirst <ovolot@gmail.com>
2023-07-25 20:29:06 +0300

    fix regex expression

---
+++ constants/regexValues.js
@@ -0,0 +1,9 @@
+const PASSWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,16})/
+const PHONE_REGEX = /^\+380\d{9}$/
+const DATE_REGEX = /^(?:19[0-9][0-9]|20[0-9][0-9])-(?:0[1-9]|1[0-2])-(?:0[1-9]|[1-2][0-9]|3[0-1])$/
+
+module.exports = {
+    PASSWD_REGEX,
+    PHONE_REGEX,
+    DATE_REGEX,
+}
