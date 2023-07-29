4b926c079b28a610877f409251e696ddcb98e272
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-07-29 20:07:21 +0300

    added search

--- controllers/notices/listNotices.js
+++ controllers/notices/listNotices.js
@@ -15,14 +15,19 @@ exports.listNotices = catchAsync(async (req, res) => {
 
   const ageFilter = [];
 
-  if (req.query.young) ageFilter.push(ageFilterEnum.youngerOneYear)
-  if (req.query.middle) ageFilter.push(ageFilterEnum.olderOneYear)
-  if (req.query.older) ageFilter.push(ageFilterEnum.olderTwoYears)
+  if (req.query.young) ageFilter.push(ageFilterEnum.youngerOneYear);
+  if (req.query.middle) ageFilter.push(ageFilterEnum.olderOneYear);
+  if (req.query.older) ageFilter.push(ageFilterEnum.olderTwoYears);
 
   const sexFilter = [];
   Object.values(petSexEnum).includes(req.query.sex)
-    ? (sexFilter.push(req.query.sex))
-    : (sexFilter.push(petSexEnum.MALE, petSexEnum.FEMALE));
+    ? sexFilter.push(req.query.sex)
+    : sexFilter.push(petSexEnum.MALE, petSexEnum.FEMALE);
+
+  let searchQuery;
+  req.query.search
+    ? (searchQuery = req.query.search.trim())
+    : (searchQuery = undefined);
 
   const { page = 1, limit = 12 } = req.query;
   const skip = (page - 1) * limit;
@@ -33,6 +38,14 @@ exports.listNotices = catchAsync(async (req, res) => {
   })
     .sort("-createdAt")
     .lean();
+
+  if (searchQuery) {
+    notices = notices.filter(
+      (item) =>
+        item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        item.comments.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }
 
   notices = notices.map((notice) => {
     if (userId) {
@@ -53,8 +66,7 @@ exports.listNotices = catchAsync(async (req, res) => {
       new Date(notice.birthday)
     );
 
-    if (ageInMonths < 12)
-      notice.category = ageFilterEnum.youngerOneYear;
+    if (ageInMonths < 12) notice.category = ageFilterEnum.youngerOneYear;
     if (ageInMonths >= 12 && ageInMonths < 24)
       notice.category = ageFilterEnum.olderOneYear;
     if (ageInMonths >= 24) notice.category = ageFilterEnum.olderTwoYears;
@@ -80,13 +92,14 @@ exports.listNotices = catchAsync(async (req, res) => {
     };
   });
 
-  if (ageFilter)
+  if (ageFilter.length > 0)
     notices = notices.filter((notice) => ageFilter.includes(notice.category));
-  
+
   notices = notices.map((notice) => {
-      notice.category = undefined
-      return notice
-  })
+    notice.category = undefined;
+
+    return notice;
+  });
 
   const paginatedNotices = notices.slice(skip, skip + limit);
 
